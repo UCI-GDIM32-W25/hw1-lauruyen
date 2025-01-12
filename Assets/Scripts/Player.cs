dc06bfb6e89133bc03a2ef72e8c3b7712d4164e8
lauruyen <lauruyen@gmail.com>
2025-01-11 19:45:44 -0800

    WASD!

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -18,11 +18,33 @@ public class Player : MonoBehaviour
 
     private void Update()
     {
+        if (Input.GetKey(KeyCode.W))
+        {
+            _playerTransform.Translate(Vector3.up*_speed*Time.deltaTime);
+        }
+        
+        if (Input.GetKey(KeyCode.A))
+        {
+            _playerTransform.Translate(Vector3.left*_speed*Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _playerTransform.Translate(Vector3.down*_speed*Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _playerTransform.Translate(Vector3.right*_speed*Time.deltaTime);
+        }
         
     }
 
     public void PlantSeed ()
     {
-        
+       if (Input.GetKey(KeyCode.Space))
+       {
+        _numSeeds--;
+       } 
     }
 }
